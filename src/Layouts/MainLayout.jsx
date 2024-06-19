6041a2b3fda04475ec0301b4a26182bcdb206d20
avishek195 <avishekmistry420@gmail.com>
2024-06-19 21:50:12 +0530

    add menu item

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,28 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
+import manuBar from "../assets/menu.png";
 import "./mainlayout.css";
 const MainLayout = ({ children }) => {
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="main-container">
       <header className="navbar-container">
         <div className="logo-section">
           <h1>Portfolio</h1>
         </div>
-
-        <ul className="nav-links">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+        {clicked ? (
+          <ul className="nav-links">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <img
+                src={manuBar}
+                alt="menu"
+                onClick={() => setClicked((prev) => !prev)}
+              />
+            </li>
+          </ul>
+        ) : (
+          <div className="nav-links">
+            <img
+              src={manuBar}
+              alt="menu"
+              onClick={() => setClicked((prev) => !prev)}
+            />
+          </div>
+        )}
       </header>
       {children}
       <footer className="footer">

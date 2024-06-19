f9bd18d37d06090fa0767149105b732f8d6ee538
avishek195 <avishekmistry420@gmail.com>
2024-06-19 22:49:07 +0530

    complete project section

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -3,6 +3,8 @@ import jokeTeller from "../../assets/images/joke_teller.png";
 import pokemon from "../../assets/images/Pokemon.png";
 import musicPlayer from "../../assets/images/music-player.png";
 import ecommerceSite from "../../assets/images/shooping-site.png";
+import coustomCountdown from "../../assets/images/custom-countdown.png";
+import pictureInPicture from "../../assets/images/picture-in-picture.png";
 import "./project.css";
 
 const Project = () => {
@@ -30,6 +32,20 @@ const Project = () => {
       imgSrc: ecommerceSite,
       sourceLink: "https://github.com/avishek195/React-Card-Project",
       liveLink: "https://eclectic-torrone-365ea9.netlify.app/",
+    },
+    {
+      title: "Custom Countdown",
+      imgSrc: coustomCountdown,
+      sourceLink: "https://github.com/avishek195/custom-countdown",
+      liveLink: "https://avishek195.github.io/custom-countdown/",
+    },
+    {
+      title: "Picture In Picture",
+      imgSrc: pictureInPicture,
+      sourceLink:
+        "https://github.com/avishek195/picture-in-picture-using-javascript",
+      liveLink:
+        "https://avishek195.github.io/picture-in-picture-using-javascript/",
     },
   ];
   return (
